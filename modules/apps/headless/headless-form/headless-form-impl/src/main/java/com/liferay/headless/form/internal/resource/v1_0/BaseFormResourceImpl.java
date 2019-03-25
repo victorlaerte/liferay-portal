5e58eaa05ebf31e5202465f66fa42580ce0a5475
Javier Gamarra <nhpatt@gmail.com>
2019-03-25 15:06:04 +0100

    LPS-91859 generated

--- modules/apps/headless/headless-form/headless-form-impl/src/main/java/com/liferay/headless/form/internal/resource/v1_0/BaseFormResourceImpl.java
+++ modules/apps/headless/headless-form/headless-form-impl/src/main/java/com/liferay/headless/form/internal/resource/v1_0/BaseFormResourceImpl.java
@@ -15,11 +15,13 @@
 package com.liferay.headless.form.internal.resource.v1_0;
 
 import com.liferay.headless.form.dto.v1_0.Form;
+import com.liferay.headless.form.dto.v1_0.FormDocument;
 import com.liferay.headless.form.resource.v1_0.FormResource;
 import com.liferay.petra.function.UnsafeFunction;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
+import com.liferay.portal.vulcan.multipart.MultipartBody;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
 import com.liferay.portal.vulcan.util.TransformUtil;
@@ -102,28 +104,17 @@ public abstract class BaseFormResourceImpl implements FormResource {
 	}
 
 	@Override
-	@GET
-	@Path("/forms/{form-id}/fetch-latest-draft")
-	@Produces("application/json")
-	@Tags(value = {@Tag(name = "Form")})
-	public Form getFormFetchLatestDraft(
-			@NotNull @PathParam("form-id") Long formId)
-		throws Exception {
-
-		return new Form();
-	}
-
-	@Override
-	@Consumes("application/json")
+	@Consumes("multipart/form-data")
 	@POST
 	@Path("/forms/{form-id}/upload-file")
 	@Produces("application/json")
 	@Tags(value = {@Tag(name = "Form")})
-	public Form postFormUploadFile(
-			@NotNull @PathParam("form-id") Long formId, Form form)
+	public FormDocument postFormUploadFile(
+			@NotNull @PathParam("form-id") Long formId,
+			MultipartBody multipartBody)
 		throws Exception {
 
-		return new Form();
+		return new FormDocument();
 	}
 
 	public void setContextCompany(Company contextCompany) {
